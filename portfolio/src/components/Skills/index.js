351c40fce9c6cd87a8234fcbd88d3629a6771f84
Joshua R. Munoz <jrmunoz@Joshuas-MacBook-Pro.local>
2020-03-31 07:06:33 -0500

    skills cards hover animation done

--- portfolio/src/components/Skills/index.js
+++ portfolio/src/components/Skills/index.js
@@ -1,15 +1,52 @@
 import React, { Component } from "react";
 import { Typography } from "@material-ui/core";
 import "./Skills.css";
+import frontend from "../../images/frontend.png";
+import backend from "../../images/backend.png";
 
 class Skills extends Component {
   render() {
     return (
       <div>
         <section id="__skills">
-          <Typography variant="h1" component="h2" gutterBottom>
-            Skills Section
-          </Typography>
+          <div className="__skillsContainer">
+            <div className="__skillsCard">
+              <div className="face face1">
+                <div className="__skillsContent">
+                  <img src={frontend} id="__frontendImg"></img>
+                  <h3>Front-End</h3>
+                </div>
+              </div>
+              <div className="face face2">
+                <Typography
+                  variant="body1"
+                  gutterBottom
+                  className="__skillsTypography"
+                >
+                  My Front-end skills.
+                </Typography>
+              </div>
+            </div>
+          </div>
+          <div className="__skillsContainer">
+            <div className="__skillsCard">
+              <div className="face face1">
+                <div className="__skillsContent">
+                  <img src={backend} id="__backendImg"></img>
+                  <h3>Back-End</h3>
+                </div>
+              </div>
+              <div className="face face2">
+                <Typography
+                  variant="body1"
+                  gutterBottom
+                  className="__skillsTypography"
+                >
+                  My Back-end skills.
+                </Typography>
+              </div>
+            </div>
+          </div>
         </section>
       </div>
     );
