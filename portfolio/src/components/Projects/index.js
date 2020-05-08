84cefe16624b4cd06bb81d67c55b7bd5bb42ab05
Joshua R. Munoz <jrmunoz@Joshuas-MacBook-Pro.local>
2020-05-08 15:24:29 -0500

    projects section done

--- portfolio/src/components/Projects/index.js
+++ portfolio/src/components/Projects/index.js
@@ -4,6 +4,8 @@ import "./Projects.css";
 import qwiki from "../../images/qwiki.png";
 import GitHubIcon from "@material-ui/icons/GitHub";
 import WebIcon from "@material-ui/icons/Web";
+import bamazon from "../../images/bamazon.png";
+import portfolio from "../../images/portfolio.png";
 
 class Projects extends Component {
   render() {
@@ -30,12 +32,13 @@ class Projects extends Component {
                   to read or contribute.
                   <h4>Technologies Used</h4>
                   Axios, Bcrypt, Express, Heroku, Javascript, Material UI,
-                  Mongo, Mongoose, React, Validator
+                  Mongo, Mongoose, React, Validator, css
                   <div id="__projectlinks">
                     <div id="__projectlink">
                       <a
                         href="https://github.com/ragobash/qwiki"
                         target="_blank"
+                        id="__link"
                       >
                         <GitHubIcon />
                       </a>
@@ -44,6 +47,7 @@ class Projects extends Component {
                       <a
                         href="https://protected-forest-42016.herokuapp.com/"
                         target="_blank"
+                        id="__link"
                       >
                         <WebIcon />
                       </a>
@@ -57,8 +61,8 @@ class Projects extends Component {
             <div className="__projectsCard">
               <div className="face face1">
                 <div className="__projectsContent">
-                  <img id="__backendImg"></img>
-                  <h3>Back-End</h3>
+                  <img src={bamazon} id="__bamazonImg"></img>
+                  <h3>Bamazon</h3>
                 </div>
               </div>
               <div className="face face2">
@@ -67,14 +71,63 @@ class Projects extends Component {
                   gutterBottom
                   className="__projectsTypography"
                 >
-                  I like to write code that is effient and understandable that
-                  way it can easily be built on.
-                  <br></br>
-                  <h4>What I Write In</h4>
-                  Node, MongoDB, MySQL
-                  <br></br>
-                  <h4>What I Write With</h4>
-                  Express, Mongoose, Sequelize, Firebase, Git
+                  Bamazon is the back-end of a amazon type website. You can
+                  access manager account and view inventory as well as remove
+                  and add items. The customer account can purchase items.
+                  <h4>Technologies Used</h4>
+                  Node, SQL, Sequelize, Javascript
+                  <div id="__projectlinks">
+                    <div id="__projectlink">
+                      <a
+                        href="https://github.com/Joshmunoz63/Bamazon"
+                        target="_blank"
+                        id="__link"
+                      >
+                        <GitHubIcon />
+                      </a>
+                    </div>
+                    <div id="__projectlink">
+                      <a
+                        href="https://www.youtube.com/watch?v=AMl97U7tEBY&feature=youtu.be"
+                        target="_blank"
+                        id="__link"
+                      >
+                        <WebIcon />
+                      </a>
+                    </div>
+                  </div>
+                </Typography>
+              </div>
+            </div>
+          </div>
+          <div className="__projectsContainer">
+            <div className="__projectsCard">
+              <div className="face face1">
+                <div className="__projectsContent">
+                  <img src={portfolio} id="__portfolioImg"></img>
+                  <h3>Portfolio</h3>
+                </div>
+              </div>
+              <div className="face face2">
+                <Typography
+                  variant="body1"
+                  gutterBottom
+                  className="__projectsTypography"
+                >
+                  This is what you are looking at.
+                  <h4>Technologies Used</h4>
+                  React, Material UI, css
+                  <div id="__projectlinks">
+                    <div id="__projectlink">
+                      <a
+                        href="https://github.com/Joshmunoz63/react-portfolio"
+                        target="_blank"
+                        id="__link"
+                      >
+                        <GitHubIcon />
+                      </a>
+                    </div>
+                  </div>
                 </Typography>
               </div>
             </div>
