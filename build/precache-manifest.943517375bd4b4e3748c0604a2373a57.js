1795150cd6ca740212779bb39cefef6941c01d8e
Joshua R. Munoz <jrmunoz@Joshuas-MacBook-Pro.local>
2020-06-13 03:25:53 -0500

    github pages

---
+++ build/precache-manifest.943517375bd4b4e3748c0604a2373a57.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a66698f3ea6d380893ae1ec63d7a94ff",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "c6ba3d5452a81c5ee9af",
+    "url": "/react-portfolio/static/css/main.3896aa4c.chunk.css"
+  },
+  {
+    "revision": "cc7c500e97f621957b19",
+    "url": "/react-portfolio/static/js/2.7fde9799.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/react-portfolio/static/js/2.7fde9799.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c6ba3d5452a81c5ee9af",
+    "url": "/react-portfolio/static/js/main.e67b44ce.chunk.js"
+  },
+  {
+    "revision": "fc28123deae08e53ec3a",
+    "url": "/react-portfolio/static/js/runtime-main.ae70f43c.js"
+  },
+  {
+    "revision": "f94dd9d321e13de3c6aeb5d713b68bd0",
+    "url": "/react-portfolio/static/media/backend.f94dd9d3.png"
+  },
+  {
+    "revision": "28524543c4e495cbb817fd14b717dea3",
+    "url": "/react-portfolio/static/media/bamazon.28524543.png"
+  },
+  {
+    "revision": "ec7eacb1825c4852e5d2002e8092d00c",
+    "url": "/react-portfolio/static/media/dude.ec7eacb1.png"
+  },
+  {
+    "revision": "726345bcd7ca3a23ebfc3876b09b6b38",
+    "url": "/react-portfolio/static/media/frontend.726345bc.png"
+  },
+  {
+    "revision": "eddc20b83209af51f42aa093a971df12",
+    "url": "/react-portfolio/static/media/portfolio.eddc20b8.png"
+  },
+  {
+    "revision": "c9720b2617efd7aef950ef5ad46129b9",
+    "url": "/react-portfolio/static/media/qwiki.c9720b26.png"
+  }
+]);
