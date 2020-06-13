c3817d060b1e9160efdcb6da64922b1e9426985e
Joshua R. Munoz <jrmunoz@Joshuas-MacBook-Pro.local>
2020-06-13 05:31:43 -0500

    fixed path for server maybe?

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ if (process.env.NODE_ENV === "production") {
 // Send every request to the React app
 // Define any API routes before this runs
 app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "/"));
+  res.sendFile(path.join(__dirname, "src"));
 });
 
 // Setup server listener
